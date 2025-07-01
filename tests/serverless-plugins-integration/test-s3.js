ad4e9be5cd7701cd7243f858491ccf6cb1316306
Ahniya Staravoitava <agniastar@outlook.com>
2025-07-01 14:21:17 +0200

    refactor: replace hardcoded file path with a constant in S3 test file for improved maintainability

--- tests/serverless-plugins-integration/test-s3.js
+++ tests/serverless-plugins-integration/test-s3.js
@@ -5,6 +5,9 @@ const onExit = require('signal-exit');
 const pump = require('pump');
 const {delay, getSplitLinesTransform} = require('./utils');
 
+const EXPECTED_LAMBDA_CALL = 9; // pictures files are consumed twice, by myPromiseHandler and myPythonHandler
+const PATH = './files/test.txt';
+
 const client = new Minio.Client({
   region: 'eu-west-1',
   endPoint: 'localhost',
@@ -14,24 +17,22 @@ const client = new Minio.Client({
   useSSL: false
 });
 
-const path = './files/test.txt';
 const uploadFiles = async () => {
   await delay(1000);
 
   await Promise.all([
-    client.fPutObject('documents', 'first.txt', path),
-    client.fPutObject('pictures', 'first.txt', path),
-    client.fPutObject('files', 'first.txt', path),
-    client.fPutObject('documents', 'second.txt', path),
-    client.fPutObject('pictures', 'second.txt', path),
-    client.fPutObject('files', 'second.txt', path),
-    client.fPutObject('others', 'correct/test.txt', path),
-    client.fPutObject('others', 'wrong/test.csv', path),
-    client.fPutObject('others', 'correct/test.csv', path),
-    client.fPutObject('others', 'wrong/test.txt', path)
+    client.fPutObject('documents', 'first.txt', PATH),
+    client.fPutObject('pictures', 'first.txt', PATH),
+    client.fPutObject('files', 'first.txt', PATH),
+    client.fPutObject('documents', 'second.txt', PATH),
+    client.fPutObject('pictures', 'second.txt', PATH),
+    client.fPutObject('files', 'second.txt', PATH),
+    client.fPutObject('others', 'correct/test.txt', PATH),
+    client.fPutObject('others', 'wrong/test.csv', PATH),
+    client.fPutObject('others', 'correct/test.csv', PATH),
+    client.fPutObject('others', 'wrong/test.txt', PATH)
   ]);
 };
-const EXPECTED_LAMBDA_CALL = 9; // pictures files are consumed twice, by myPromiseHandler and myPythonHandler
 
 const serverless = spawn('sls', ['offline', 'start', '--config', 'serverless.s3.yml'], {
   stdio: ['pipe', 'pipe', 'pipe'],
