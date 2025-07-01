000e0641d0cfc3dfb34c16fc49aec1330ab7130d
Ahniya Staravoitava <agniastar@outlook.com>
2025-07-01 14:46:18 +0200

    refactor: improve logging in lambda handler by adding context to log messages

--- tests/serverless-plugins-integration/lambda/handler.js
+++ tests/serverless-plugins-integration/lambda/handler.js
@@ -1,9 +1,9 @@
 module.exports.promise = (event, context) => {
-  console.log(JSON.stringify(event));
+  console.log('lambda event promise', JSON.stringify(event));
   return Promise.resolve();
 };
 
 module.exports.callback = (event, context, cb) => {
-  console.log(JSON.stringify(event));
+  console.log('lambda event callback', JSON.stringify(event));
   cb();
 };
